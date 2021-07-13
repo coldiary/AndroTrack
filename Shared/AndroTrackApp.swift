5108d8874fa04fafb4a0e5701accff99046bfe7e
Benoit Sida <benoit.sida@gmail.com>
2021-07-13 14:41:06 -0400

    Initial Commit

---
+++ Shared/AndroTrackApp.swift
@@ -0,0 +1,17 @@
+//
+//  AndroTrackApp.swift
+//  Shared
+//
+//  Created by Benoit Sida on 2021-07-13.
+//
+
+import SwiftUI
+
+@main
+struct AndroTrackApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
